bf6951a05b0723686ee166768a6a28ac216c0ee9
karanmathur <karanmathur193@gmail.com>
2024-10-17 10:45:06 -0700

    One bit attempt

--- Bonus-DeadDrop/receiver.c
+++ Bonus-DeadDrop/receiver.c
@@ -2,23 +2,38 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-
+#define BUFF_SIZE (256*1024 + 32*1024)
 int main(int argc, char **argv)
 {
     // [Bonus] TODO: Put your covert channel setup code here
-
+    volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
+    int tmp;
+    uint64_t latency[BUFF_SIZE/64] = {0}; 
+    uint64_t latency_sum = 0; 
     printf("Please press enter.\n");
 
     char text_buf[2];
     fgets(text_buf, sizeof(text_buf), stdin);
 
     printf("Receiver now listening.\n");
-
+    for(int k =0 ; k < 50; k++){
+    for (int i = 0; i < BUFF_SIZE/64; i ++) {
+                 eviction_buffer[i*64]=1;    
+        }       
+    }
     bool listening = true;
     while (listening) {
-
+	latency_sum = 0; 
         // [Bonus] TODO: Put your covert channel code here
-
+/*	for (int i = 0; i < BUFF_SIZE/64; i ++) {
+		tmp = eviction_buffer[i*64]; 	
+	}	*/
+	for (int i = 0; i < BUFF_SIZE/64; i ++) {
+                latency[i] = measure_one_block_access_time((uint64_t)(eviction_buffer + i*64));
+		latency_sum = latency_sum + latency[i]; 
+        }
+	//printf("\nlatency sum:%d\n",latency_sum); 
+	if((latency_sum/(BUFF_SIZE/64)) > 50) printf("\nlatency sum:%d\n",(latency_sum/(BUFF_SIZE/64)));
     }
 
     printf("Receiver finished.\n");
--- Bonus-DeadDrop/sender.c
+++ Bonus-DeadDrop/sender.c
@@ -2,10 +2,11 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+#include <string.h>
 
 // [Bonus] TODO: define your own buffer size
 #define BUFF_SIZE (1<<21)
-//#define BUFF_SIZE TODO
+//#define BUFF_SIZE 256*1024*2
 
 int main(int argc, char **argv)
 {
@@ -23,8 +24,13 @@ int main(int argc, char **argv)
     // Thus, we use a dummy write here to trigger page allocation
     // so later access will not suffer from such overhead.
     *((char *)buf) = 1; // dummy write to trigger page allocation
-
-
+    
+    
+    
+    volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
+    //uint64_t latency[256*4] = {0};  
+    int one_bit = 0; 
+    int tmp; 
     // [Bonus] TODO:
     // Put your covert channel setup code here
 
@@ -34,10 +40,27 @@ int main(int argc, char **argv)
     while (sending) {
         char text_buf[128];
         fgets(text_buf, sizeof(text_buf), stdin);
-
+	one_bit = 0; 
         // [Bonus] TODO:
         // Put your covert channel code here
-    
+        one_bit = strcmp(text_buf, "0\n");
+	if(one_bit == 0){
+		printf("IT IS 0");
+		// evict entire L2
+		for (int k = 0; k < 50; k++) {
+		for(int i = 0; i < BUFF_SIZE*1.9/64; i++){
+			
+			tmp = eviction_buffer[i*64]; 		
+		}
+		}
+	}
+	else{
+		//evict only 25% of L2 
+		for (int i =0; i < BUFF_SIZE/(64*4); i++){
+			tmp = eviction_buffer[i*64]; 
+		}
+	}
+	sending = false; 
     }
 
     printf("Sender finished.\n");
