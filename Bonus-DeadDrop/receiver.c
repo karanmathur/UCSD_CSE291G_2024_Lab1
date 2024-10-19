36af88b0986d7bfadba83db1be64e7c41eadb1f3
karanmathur <karanmathur193@gmail.com>
2024-10-18 19:19:23 -0700

    Attempted 8 bit version, perhaps cache is thrashing

--- Bonus-DeadDrop/receiver.c
+++ Bonus-DeadDrop/receiver.c
@@ -2,13 +2,15 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-#define BUFF_SIZE (256*1024 + 32*1024)
+#define BUFF_SIZE (256*1024)
 int main(int argc, char **argv)
 {
     // [Bonus] TODO: Put your covert channel setup code here
     volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
     int tmp;
-    uint64_t latency[BUFF_SIZE/64] = {0}; 
+    int total = 0; 
+    uint64_t latency = 0;
+   // uint64_t latency_freq[BUFF_SIZE/64] = {0}; 
     uint64_t latency_sum = 0; 
     printf("Please press enter.\n");
 
@@ -17,7 +19,7 @@ int main(int argc, char **argv)
 
     printf("Receiver now listening.\n");
     for(int k =0 ; k < 50; k++){
-    for (int i = 0; i < BUFF_SIZE/64; i ++) {
+    for (int i = 4095; i >= 0; i --) {
                  eviction_buffer[i*64]=1;    
         }       
     }
@@ -25,16 +27,37 @@ int main(int argc, char **argv)
     while (listening) {
 	latency_sum = 0; 
         // [Bonus] TODO: Put your covert channel code here
-/*	for (int i = 0; i < BUFF_SIZE/64; i ++) {
+	uint64_t latency_freq[BUFF_SIZE/64] = {0};
+	
+	for (int i = 0; i < BUFF_SIZE/64; i++){ // BUFF_SIZE/64 = 4096 lines, also probing in opposite direction 
+		latency = measure_one_block_access_time((uint64_t)(eviction_buffer + i*64));
+		if(latency > 50) latency_freq[i]++; 
+	}
+	int jval = 0; 
+	for (int i = 0; i < BUFF_SIZE/1024; i++){ // need to check 4 lines in 4 sets 
+		total = 0;
+	        for(int j= 0; j <4; j++){	
+		if(latency_freq[i + 1024*j ] == 1 ) total++; 
+		if(latency_freq[i + 256 + 1024*j] == 1) total++; 
+		if(latency_freq[i + 512 + 1024*j] == 1) total++; 
+		if(latency_freq[i + 768 + 1024*j] == 1) total++; 
+		//if(total >=3) printf("\n%d\n",i); 
+		jval =j; 
+	}
+		if(total >= 9) printf("\n%d,%d\n",i,jval);
+	}
+	
+	/*	for (int i = 0; i < BUFF_SIZE/64; i ++) {
 		tmp = eviction_buffer[i*64]; 	
 	}	*/
-	for (int i = 0; i < BUFF_SIZE/64; i ++) {
+/*	for (int i = 0; i < BUFF_SIZE/64; i ++) {
                 latency[i] = measure_one_block_access_time((uint64_t)(eviction_buffer + i*64));
 		latency_sum = latency_sum + latency[i]; 
         }
 	//printf("\nlatency sum:%d\n",latency_sum); 
 	if((latency_sum/(BUFF_SIZE/64)) > 50) printf("\nlatency sum:%d\n",(latency_sum/(BUFF_SIZE/64)));
-    }
+*/  
+      	}
 
     printf("Receiver finished.\n");
 
--- Bonus-DeadDrop/sender.c
+++ Bonus-DeadDrop/sender.c
@@ -3,10 +3,38 @@
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
 #include <string.h>
+#include <math.h> 
 
 // [Bonus] TODO: define your own buffer size
 #define BUFF_SIZE (1<<21)
 //#define BUFF_SIZE 256*1024*2
+
+int extract_numerical_value(const char *char_array) {
+    int length = strlen(char_array);
+
+    // Ensure the character array is not empty
+    if (length == 0) {
+        printf("Error: Empty character array.\n");
+        return -1;
+    }
+
+    // Check if the first character is a digit
+    if (char_array[0] < '0' || char_array[0] > '9') {
+        printf("Error: Invalid character at the beginning.\n");
+        return -1;
+    }
+
+    // Convert the numerical part of the string to an integer
+    int value = atoi(char_array);
+
+    // Check if the value is within the valid range (0 to 255)
+    if (value < 0 || value > 255) {
+        printf("Error: Value out of range (0-255).\n");
+        return -1;
+    }
+
+    return value;
+}
 
 int main(int argc, char **argv)
 {
@@ -30,20 +58,43 @@ int main(int argc, char **argv)
     volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
     //uint64_t latency[256*4] = {0};  
     int one_bit = 0; 
-    int tmp; 
+    int tmp;
+    int set_no=0;  
+    int index = 0; 
+    //uint64_t binary[8] = {0};
+    int len = 128; 
+    char *binary = malloc(len * 8 + 1);
     // [Bonus] TODO:
     // Put your covert channel setup code here
 
     printf("Please type a message.\n");
-
+	char text_buf[128];
+        fgets(text_buf, sizeof(text_buf), stdin);
     bool sending = true;
     while (sending) {
-        char text_buf[128];
-        fgets(text_buf, sizeof(text_buf), stdin);
-	one_bit = 0; 
+        //char text_buf[128];
+        //fgets(text_buf, sizeof(text_buf), stdin);
+	//one_bit = 0; 
         // [Bonus] TODO:
         // Put your covert channel code here
-        one_bit = strcmp(text_buf, "0\n");
+//	*binary = string_to_binary(text_buf);
+//       printf("\nbinary:%d\n",*binary);
+
+/*       for(int i =0; i<8;i++){
+       		set_no = set_no + binary[i]*(pow(2,i)); //map the 8 bit number from one of set 0 to 255 
+       }
+*/
+	set_no = extract_numerical_value(text_buf);	
+	//printf("\n%d\n",set_no);
+       for (int i = 0; i<4; i++){ // filling all 4 ways of that set
+	       for (int j = 0; j<4; j++) {
+		eviction_buffer[((set_no+256*j)*64) + 1024*64*i] = 1; // since there are 1024 sets in L2, fill total 4 sets that correspond to set no. or set_no + multiple of 256
+       }
+       }
+
+
+
+       /* one_bit = strcmp(text_buf, "0\n");
 	if(one_bit == 0){
 		printf("IT IS 0");
 		// evict entire L2
@@ -60,7 +111,9 @@ int main(int argc, char **argv)
 			tmp = eviction_buffer[i*64]; 
 		}
 	}
-	sending = false; 
+	sending = false;
+       */ 	
+       //sending = false; 
     }
 
     printf("Sender finished.\n");
